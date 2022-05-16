af6265fb3ca4ca26fa0ff22923ac0ed78fb7fca5
bvs3040 <taga-si@mail.ru>
2022-05-16 13:08:41 +0700

    task_5

--- task_5/task_5.js
+++ task_5/task_5.js
@@ -1,18 +1,23 @@
 const wsUrl = 'wss://ws.ifelse.io';
+let i ; // флаг для блокировки вывода ответа сервера
 
 function pageLoaded() {
     const btnCoord = document.querySelector('.btn-coord');
     const btnSend = document.querySelector('.btn-send');
     const inputTextField = document.querySelector('input');
     const outputMesage = document.querySelector('.msg-block');
-
+    
     let websocket = new WebSocket(wsUrl);
-
+    
     websocket.onopen = () => {
         writeMessage("Соединение установлено. Привет!", true);
+        i = 0; // при установке соединения приходит ответ. его не выводим
     }
 
-    let i = 0; // не выводим ответ
+    websocket.onclose = function() {
+        writeMessage("Соединение закрыто. До свидания!", true);
+    };
+    
     websocket.onmessage = (event) => {
         if (i) writeMessage(event.data, true); 
         i++; 
@@ -21,17 +26,18 @@ function pageLoaded() {
     websocket.onerror = () => {
         writeMessage("При передаче данных произошла ошибка!", true);
     }
-
+    
     btnSend.addEventListener('click', () => {
         if (!inputTextField.value) return;
         websocket.send(inputTextField.value);
         writeMessage(inputTextField.value, false);
         inputTextField.value = "";
     })
-
+    
     function writeMessage(message, flag) {
-        let messageText = `<p class="${flag ? "outputMessage" : "inputMessage"}"> ${message} </p>`;
+        let messageText = `<div class="${flag ? "outputMessage" : "inputMessage"}"> ${message} </div>`;
         outputMesage.innerHTML += messageText;
+        outputMesage.scrollTop = outputMesage.scrollHeight;
     }
 
     btnCoord.addEventListener('click', () => {
@@ -39,13 +45,16 @@ function pageLoaded() {
             writeMessage('Информация о местоположении недоступна', false);
         }
         const success = (position) => {
-            const {coords} = position;
-            const link = `<a href="https://www.openstreetmap.org/#map=12/${coords.latitude}/${coords.longitude}" target="_blank">Вы находитесь здесь</a>`;
+            const {coords} = position; // честно не сосем понимаю синтаксис
+            const link = `<a href="https://www.openstreetmap.org/?mlat=${coords.latitude}&mlon=${coords.longitude}#map=12/${coords.latitude}/${coords.longitude}" target="_blank">Вы находитесь здесь</a>`;
             writeMessage(link, false);
-            i=0;// не выводим ответ
-            websocket.send(link);
+            // блокировка ответа при частом клике по "btnCoord"
+            if (i) {
+                websocket.send(link);
+                i=0;
+            }
         }
-
+        
         if (!navigator.geolocation) {
             writeMessage('Браузер не поддерживает геолокацию', false);
         } else {
